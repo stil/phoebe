fc8ba3188dd82de39ee6251b59f6db6b3939889d
stil <przemekryciuk@gmail.com>
2013-08-16 22:59:24 +0200

    Added getSource() and isInChannel() to event.

--- src/Phoebe/Event/Command.php
+++ src/Phoebe/Event/Command.php
@@ -51,4 +51,44 @@ class Command extends Event
     {
         return $this->logger;
     }
+
+    /**
+     * Determines whether a given string is a valid IRC channel name.
+     *
+     * @param string $string String to analyze
+     *
+     * @return bool TRUE if $string contains a valid channel name, FALSE
+     *         otherwise
+     */
+    protected function isChannelName($string)
+    {
+        // Per the 2000 RFCs 2811 and 2812, channels may begin with &, #, +, or !
+        return (strspn($string, '#&+!', 0, 1) >= 1);
+    }
+
+    /**
+     * Returns the channel name if the event occurred in a channel or the
+     * user nick if the event was a private message directed at the bot by a
+     * user.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        $msg = $this->getMessage();
+        
+        if (!empty($msg['targets'])) {
+            return $msg['targets'][0];
+        }
+    }
+
+    /**
+     * Returns whether or not the event occurred within a channel.
+     *
+     * @return TRUE if the event is in a channel, FALSE otherwise
+     */
+    public function isInChannel()
+    {
+        return $this->isChannelName($this->getSource());
+    }
 }
